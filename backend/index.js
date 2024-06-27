79a8ce6f44f6cc2c13a99f1f351c8bf4da2ca28d
ROOPEESH_KUMAR <128828453+roopeshkumar11@users.noreply.github.com>
2024-06-28 01:03:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,17 @@ const dotenv =require("dotenv")
 dotenv.config()
 const app=express()
 app.use(express.json())
-app.use(cors({
-    origin:["https://production-mern-2l67frontend.vercel.app/"],
-    methods:["POST","GET"],
-    credentials:true
-})); 
+// app.use(cors({
+//     origin:["https://production-mern-2l67frontend.vercel.app/"],
+//     methods:["POST","GET"],
+//     credentials:true
+// })); 
+const corsOptions = {
+  origin: 'https://production-mern-2l67frontend.vercel.app',
+  optionsSuccessStatus: 200 // For legacy browser support
+};
+
+app.use(cors(corsOptions));
 const PORT=process.env.PORT || 4001
 const URI=process.env.MonogoDBURI
 
