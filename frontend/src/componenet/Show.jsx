32b6ee7556f7485673acab167257465a63affc94
ROOPEESH_KUMAR <128828453+roopeshkumar11@users.noreply.github.com>
2024-06-28 00:57:41 +0530

    Update Show.jsx

--- frontend/src/componenet/Show.jsx
+++ frontend/src/componenet/Show.jsx
@@ -12,7 +12,7 @@ function Show() {
   
 
   useEffect(()=>{
-    axios.get("production-mern-amber.vercel.app/show")
+    axios.get("https://production-mern-amber.vercel.app/show")
     .then((reponse)=>{
       
       setdata(reponse.data)
